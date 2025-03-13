74fda260ab5d6b9f7ce3394b41ae13ba417bc6b2
Sandip Hari <94623134+Sandiphari03@users.noreply.github.com>
2025-03-13 16:52:51 +0530

    Update ConflictApplication.java

--- src/main/java/com/git_conflict/conflict/ConflictApplication.java
+++ src/main/java/com/git_conflict/conflict/ConflictApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConflictApplication {
 
 	public static void main(String[] args) {
+		String x ="sandip";
 		SpringApplication.run(ConflictApplication.class, args);
 	}
 
